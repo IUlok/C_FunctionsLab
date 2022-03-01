8b8a6daaee6c1879a2bf3e938814d5650560c65f
Ulok <elfbrus@gmail.com>
2022-03-02 00:44:57 +0300

    комменты

--- func.c
+++ func.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include "funcs.h"
+#include "funcs.h" // Заг. файл для функции интегрирования
 
 int main() {
   int kolvo_shagov;
   printf("Введите точность (кол-во шагов): ");
   scanf("%d", &kolvo_shagov);
 
-  double *f = (double*)malloc(kolvo_shagov*sizeof(double));
+  double *f = (double*)malloc(kolvo_shagov*sizeof(double)); // Дин. Массив с выделенной памятью и кол-вом ячеек в соотв. с кол-вом шагов
   float shag;
-  int num = 0;
-  double S1, S2, S3;
+  int num = 0; // Индекс элементов массива
+  double S1, S2, S3; // Площадь в промежутках
 
-  shag=fabs((-1.2)-(-0.8))/kolvo_shagov;
-  for (float i = -1.2; i<=-0.8; i+=shag) {
+  shag=fabs((-1.2)-(-0.8))/kolvo_shagov; // Величина шага - модуль разности границ и количества шагов
+  for (float i = -1.2; i<=-0.8; i+=shag) { // Обработка первого промежутка
     f[num] = pow(fabs(i), 3);
     num++;
   }
-  S1 = Integral(f, shag, num);
+  S1 = Integral(f, shag, num); // Ф-ция счёта интеграла
   printf("Участок 1 = %lf\n", S1);
   num = 0;
 
-  shag=fabs((-0.8)-(0))/kolvo_shagov;
+  shag=fabs((-0.8)-(0))/kolvo_shagov; // Обработка вторгоо промежутка
   for (float i = -0.8; i<=0; i+=shag) {
     f[num] = fabs(i);
     num++;
@@ -31,7 +31,7 @@ int main() {
   printf("Участок 2 = %lf\n", S2);
   num = 0;
 
-  shag=fabs((0)-(1.8))/kolvo_shagov;
+  shag=fabs((0)-(1.8))/kolvo_shagov; // Обработка третьего промежутка
   for (float i = 0.000001; i<=1.8; i+=shag) {
     f[num] = log(fabs(i))+1;
     num++;
@@ -41,6 +41,6 @@ int main() {
   num = 0;
 
   printf("Сумма всей площади под графиком: %lf\n", (S1+S2+S3));
-  free (f);
+  free (f); // Освобождение памяти
   return 0;
 }
--- funcs.h
+++ funcs.h
@@ -1,8 +1,8 @@
 double Integral(double *f, float shag, int num) {
-  double res = 0;
+  double res = 0; // Счетчик площади
   for (int i=0; i<=num; i++) {
-    res+=f[i];
+    res+=f[i]; // Сложение всех участков
   }
-  res *= shag;
+  res *= shag; // Аналог S*h
   return res;
 }
